59b6f75e750686d15d9643e5c0583399a8a032c2
itliwei <itliwei@163.com>
2021-09-11 02:14:27 +0800

    添加目录

--- .vuepress/plugins/read-time/index.js
+++ .vuepress/plugins/read-time/index.js
@@ -87,7 +87,7 @@ module.exports = (options = {}) => ({
             }
 
             // 由于README.md会显示在GitHub上，设置frontmatter不好看，改为这里进行
-            frontmatter.title = "CTO修炼之路：构筑可靠的大型分布式系统"
+            frontmatter.title = "CTO修炼之路：不想当CTO的码农不是好码农"
             frontmatter.comment = false
             frontmatter.pageClass = "index-page-class"
         }
